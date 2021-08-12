da7b18f5f698455ed12ed30029f1deab46b57892
Kundan Kumar <kundankr94700@gmail.com>
2021-08-12 16:40:27 +0530

    redux demo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore} from 'redux'
+
+let store=createStore(counter)
+const increment=()=>{
+  return {
+    type:"INCREMENT"
+  }
+}
+const decrement=()=>{
+  return{
+    type:"DECREMENT"
+
+  }
+}
+const counter=(state=0,action)=>{
+  switch(action.type){
+    case "INCREMENT":
+      return state+1
+      case "DECREMENT":
+        return state-1
+  }
+}
+//store
+let store=createStore(counter) /// to displayour counter in the console window
+store.subscribe(()=>console.log(store.getState()));
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(crement());
 ReactDOM.render(
   <React.StrictMode>
     <App />
